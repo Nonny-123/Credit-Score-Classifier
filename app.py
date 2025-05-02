a4334bfe863d9633b0a14103e5d1638db817949b
Nonny-123 <nonnyokonji@gmail.com>
2025-05-02 23:49:39 +0100

    flask app commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from app.routes import routes
+import logging
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(routes)
+
+    logging.basicConfig(level=logging.INFO)  
+    return app
+
+app = create_app() 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
